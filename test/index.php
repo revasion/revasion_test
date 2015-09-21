bb25903c822979482c7863e4b51fad32ca519e66
Radoslav Georgiev <Radoslav Georgiev@RadoslavGeorgie>
2015-09-21 13:16:20 +0300

    dsad

---
+++ test/index.php
@@ -0,0 +1,16 @@
+<?php
+//use Guest\Mest\Test\ClassTest;
+//namespace Guest\Mest\Test;
+//require 'ClassTest.php';
+//$c=new ClassTest();
+//$c->set(1);
+//$c->get();
+//$m=new DateTime();
+//echo $m->getTimestamp();
+
+require_once  "Cnain.php";
+$ch=new Chain();
+echo $ch->add2()->add5()->num;
+//echo $ch->num;
+		
+?>
